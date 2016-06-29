50576fe35bddb22be4ab3264fc6ccf924faa6cd9
Bhing Järnefelt <bhing@tripwell.com>
2016-06-29 18:11:09 +0200

    Update submitting of form

--- js/index.js
+++ js/index.js
@@ -2,27 +2,32 @@ $(function() {
     var $form = $('#survey_form'),
 
     submitForm = function(e) {
-        var baseURL = 'https://docs.google.com/forms/d/1H0hNXPhzfML1uhMHKrWwpyInXzzzdoyUl549Xy2wHYQ/formResponse?',
-            a = 'entry.1023555150=' + $('[name="reason"]:checked').val(),
-            b = 'entry.474233524=' + $('[name="num_of_seats"]:checked').val(),
-            c = 'entry.249830821=' + $('[name="budget"]').val(),
-            d = 'entry.730655057=' + $('[name="importance"]:checked').val(),
-            e = 'entry.705807753=' + $('[name="considerations"]').val(),
-            f = 'entry.291980293=' + $('[name="contact_info"]').val(),
-            submitRef = 'submit=Submit',
-            submitURL = baseURL + [a, b, c, d, e, f, submitRef].join('&'),
-            form = document.createElement('form');
+        var reason = $('[name="reason"]:checked').val(),
+            num_of_seats = $('[name="num_of_seats"]:checked').val(),
+            budget = $('[name="budget"]').val(),
+            importance = $('[name="importance"]:checked').val(),
+            considerations = $('[name="considerations"]').val(),
+            contact_info = $('[name="contact_info"]').val();
 
         e.preventDefault();
 
-        form.action = submitURL;
-        form.method = 'POST';
-        form.target = 'no-target';
-        form.style.display = 'none';
-        form.submit(); // OK
-        form.remove();
-
-        console.log('Submission succes!');
+          var baseURL = 'https://docs.google.com/forms/d/1H0hNXPhzfML1uhMHKrWwpyInXzzzdoyUl549Xy2wHYQ/formResponse?';
+          var a = 'entry.1023555150=' + reason;
+          var b = 'entry.474233524=' + num_of_seats;
+          var c = 'entry.249830821=' + budget;
+          var d = 'entry.730655057=' + importance;
+          var e = 'entry.705807753=' + considerations;
+          var f = 'entry.291980293=' + contact_info;
+          var submitRef = 'submit=Submit';
+          var submitURL = baseURL + [a, b, c, d, e, f, submitRef].join('&');
+          var form = document.createElement('form');
+          form.action = submitURL;
+          form.method = 'POST';
+          form.target = 'no-target';
+          form.style.display = 'none';
+          form.submit(); // OK
+          console.log('Submission succes: ');
+          form.remove();
     };
 
     $form.on('submit', submitForm);
